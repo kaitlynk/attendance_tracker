3f4a0b505cadff964439619b214887f6d48aeb02
mat297 <mat297@cornell.edu>
2015-01-16 21:26:46 -0500

    pretty sure nothing was overwritten but uhhh
    i don’t even know anymore.  plz make sure i didn’t get rid of any of
    your stuff

--- require/student_header.php
+++ require/student_header.php
@@ -1,5 +1,4 @@
-<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-<script src = "js/main.js"></script>
+<script src = "js/student_main.js"></script>
 
 <div id = "header" class = "left border-box">
 	<div id = "menu-title" class = "white">Office Hours Tracker</div>
